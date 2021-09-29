5faada14733bb5f1e678d062caf57e04ec2c334f
Wolfskraehe <seuk87@gmail.com>
2021-09-29 20:05:14 +0200

    Added relative control option; changed filestructure

--- scripts/Map_Screen.gd
+++ scripts/Map_Screen.gd
@@ -4,3 +4,8 @@ extends Node2D
 
 func _on_Button_pressed():
 	Global.goto_scene("res://scenes/Instance.tscn")
+	
+func _on_Button2_pressed():
+	Global.goto_scene("res://maps/Map01.tscn")
+
+
--- scripts/Player_Ship.gd
+++ scripts/Player_Ship.gd
@@ -7,14 +7,15 @@ const ProjectileDouble =preload("res://scenes/Projectile_double_big.tscn")
 
 enum States {IDLE, SHOOT_LEFT, SHOOT_RIGHT, SHOOT_DOUBLE}
 
+export var rotation_dir : float=0
+export var rotation_speed : float = 200
+export var friction : float = 0.01
+export var acceleration : float = 0.05
+export var mouse_sensitivity := 0.05
+export var relative_controls: bool =true
+
 var _state : int = States.IDLE
 var velocity = Vector2()
-export var rotation_dir=0
-export var rotation_speed = 200
-export var friction = 0.01
-export var acceleration = 0.05
-export var mouse_sensitivity := 0.05
-
 const MAX_FIRE_DELAY=0.3
 var fire_delay=0.0
 
@@ -23,21 +24,8 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) 
 	add_to_group("player")
 			
-#function for direct input without acceleration and friction
-func get_input():
-	# rotate and move
-	rotation_dir = 0
-	velocity = Vector2()
-	if Input.is_action_pressed("right"):
-		rotation_dir += 1
-	if Input.is_action_pressed('left'):
-		rotation_dir -= 1
-	if Input.is_action_pressed('up'):
-		velocity -= get_node("gun_position").global_transform.y * speed
-	if Input.is_action_pressed("down"):
-		velocity += get_node("gun_position").global_transform.y * speed
 
-#pseudo-newtonian movement with accleration and slow deccelration
+#pseudo-newtonian movement with accleration and slow deccelration, relative movement
 func get_newtonian_input(delta):
 	if Input.is_action_pressed("right"):
 		rotation_degrees+=rotation_speed*delta
@@ -54,6 +42,24 @@ func get_newtonian_input(delta):
 		velocity=velocity.linear_interpolate(-get_node("gun_position").global_transform.y,acceleration)
 	else:
 		velocity=velocity.linear_interpolate(Vector2(0,0),friction)
+
+#verion with fixed movement	
+func get_newtonian_input_fixed(delta):
+	if Input.is_action_pressed("right"):
+		rotation_degrees+=rotation_speed*delta
+	if Input.is_action_pressed('left'):
+		rotation_degrees-=rotation_speed*delta
+	var move_dir=Vector2(1,0).rotated(rotation)
+	if Input.is_action_pressed('up'):
+		velocity=velocity.linear_interpolate(get_node("gun_position").transform.y,acceleration)
+	if Input.is_action_pressed("strafe_left"):
+		velocity=velocity.linear_interpolate(get_node("gun_position").transform.x,acceleration)
+	if Input.is_action_pressed("strafe_right"):
+		velocity=velocity.linear_interpolate(-get_node("gun_position").transform.x,acceleration)
+	if Input.is_action_pressed("down"):
+		velocity=velocity.linear_interpolate(-get_node("gun_position").transform.y,acceleration)
+	else:
+		velocity=velocity.linear_interpolate(Vector2(0,0),friction)
 		
 	
 #get input for rotation via mouse
@@ -63,38 +69,32 @@ func _input(event: InputEvent) -> void:
 	var mouse_motion = event as InputEventMouseMotion
 	if mouse_motion:
 		rotation_degrees += mouse_motion.relative.x * mouse_sensitivity
-
-	
 	
 func shoot():
 	match _state:
 		States.SHOOT_LEFT:
-			print("left")
 			if fire_delay<=0.0:
 				var p = Projectile.instance()
 				owner.add_child(p)
 				p.transform = get_node("gun_position").global_transform
 				fire_delay=MAX_FIRE_DELAY
 		States.SHOOT_RIGHT:
-			print("right")
 			if fire_delay<=0.0:
 				var p = ProjectileRight.instance()
 				owner.add_child(p)
 				p.transform = get_node("gun_position").global_transform
 				fire_delay=MAX_FIRE_DELAY
 		States.SHOOT_DOUBLE:
-			print("double")
 			if fire_delay<=0.0:
 				var p = ProjectileDouble.instance()
 				owner.add_child(p)
 				p.transform = get_node("gun_position").global_transform
 				fire_delay=MAX_FIRE_DELAY
 		States.IDLE:
-			print("idle")
 			return
 
-func _physics_process(delta):
-	#consume fire inputs:
+#consume fire inputs:
+func fire_inputs():
 	if Input.is_action_just_released("fire"):
 		if _state == States.SHOOT_LEFT:
 			_state = States.IDLE
@@ -119,14 +119,15 @@ func _physics_process(delta):
 			_state = States.SHOOT_RIGHT
 		elif _state == States.SHOOT_LEFT:
 			_state = States.SHOOT_DOUBLE
+
+func _physics_process(delta):
+	fire_inputs()	
 	
-	#get_input()
-	#shoot()
-	#rotation+=rotation_dir*rotation_speed*delta
-	#velocity = move_and_slide(velocity)
-	#fire_delay-=delta
-	
-	get_newtonian_input(delta)
+	if relative_controls:
+		get_newtonian_input(delta)
+	else:
+		get_newtonian_input_fixed(delta)
+		
 	shoot()
 	fire_delay-=delta
 	global_position-=move_and_slide(velocity*speed*delta)
