c94d32570934e5de1a2255f5c19996ca4088deb1
Daniel Kleissl <daniel.kleissl@gmx.at>
2021-10-22 19:23:27 +0200

    changed fire control to use functions instead of signals

--- scripts/Player_Ship.gd
+++ scripts/Player_Ship.gd
@@ -30,6 +30,8 @@ var double_flag = false
 
 var camera=null
 
+
+
 func _ready():
 	#make mouse invisible and confine to window
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -74,93 +76,35 @@ func get_newtonian_input_fixed(delta):
 		
 	
 #get input for rotation via mouse
-func _input(event: InputEvent) -> void:
+
 	
+	
+	
+	
+func _unhandled_input(event: InputEvent) -> void:
 	var mouse_motion = event as InputEventMouseMotion
 	if mouse_motion:
 		rotation_degrees += mouse_motion.relative.x * mouse_sensitivity
 	
-	
-#func _unhandled_input(event: InputEvent) -> void:
-#
-#	if event is InputEventMouseButton:
-#		if event.is_action_pressed("secondary_fire"):
-#			right_flag = true
-#			if left_flag:
-#				print("double")
-#			else:
-#				print("right")
-#		elif event.is_action_pressed("fire"):
-#			left_flag = true
-#			if right_flag:
-#				print("double")
-#			else:
-#				print("right")
-#		elif event.is_action_released("strafe_left")
-#		elif event.is_action_released("strafe_left")
-			
-const FIRE : Dictionary = {
-	NONE = 0,
-	LEFT = 1,
-	RIGHT = 2,
-	BOTH = 3
-}
+	var zoom_factor=camera.get_zoom()
+	if event.is_action("scroll_down") and zoom_factor <= MAX_ZOOM_FACTOR:
+		zoom_factor+=Vector2(0.1,0.1)
+		camera.set_zoom(zoom_factor)
+	if event.is_action("scroll_up") and zoom_factor >= MIN_ZOOM_FACTOR:
+		zoom_factor-=Vector2(0.1,0.1)
+		camera.set_zoom(zoom_factor)
+	if event is InputEventMouseButton:
+		if Input.is_action_just_released("secondary_fire") or Input.is_action_just_released("fire"):
+			Weapons.stop_fire()
 
-var fire : int = FIRE.NONE
-var last_fire : int = FIRE.NONE
+		if Input.is_action_pressed("fire") and Input.is_action_pressed("secondary_fire"):
+			Weapons.stop_fire()
+			Weapons.fire_double()
+		elif Input.is_action_pressed("fire"):
+			Weapons.fire_left()
+		elif Input.is_action_pressed("secondary_fire"):
+			Weapons.fire_right()
 
-
-func _process(delta: float) -> void:
-	fire_inputs()
-	
-
-func fire_inputs() -> void:
-	# credit to Whitehshampoo of godot engine discord
-	var new_fire = FIRE.NONE
-	if Input.is_action_pressed("fire"):
-		new_fire |= FIRE.LEFT
-	if Input.is_action_pressed("secondary_fire"):
-		new_fire |= FIRE.RIGHT
-	
-	if new_fire == last_fire:
-		fire = FIRE.NONE
-		return
-	
-	last_fire = new_fire
-	fire = new_fire
-	
-	test_fire_inputs()
-
-
-func test_fire_inputs() -> void:
-	_previous_state = _state
-	match fire:
-		FIRE.NONE:
-			emit_signal("stop_fire_double")
-			emit_signal("stop_fire_right")
-			emit_signal("stop_fire_left")
-			_state = States.IDLE
-			print("none")
-		FIRE.LEFT:
-			emit_signal("stop_fire_double")
-			#emit_signal("stop_fire_right")
-			_state = States.SHOOT_LEFT
-			print("left")
-		FIRE.RIGHT:
-			_state = States.SHOOT_RIGHT
-			emit_signal("stop_fire_double")
-			#emit_signal("stop_fire_left")
-			print("right")
-		FIRE.BOTH:
-			_state = States.SHOOT_DOUBLE
-			if _previous_state == States.SHOOT_RIGHT:
-				emit_signal("stop_fire_right")
-			if  _previous_state == States.SHOOT_LEFT:
-				emit_signal("stop_fire_left")
-			print("both")
-		_:
-			prints("error (%s)" % fire)
-	WeaponController.shoot(_state)
 		
 """func fire_inputs():
 	if Input.is_action_just_released("secondary_fire"):
@@ -197,25 +141,15 @@ func test_fire_inputs() -> void:
 		_state = States.IDLE
 """
 
-func _unhandled_input(event: InputEvent) -> void:
-	var zoom_factor=camera.get_zoom()
-	if event.is_action("scroll_down") and zoom_factor <= MAX_ZOOM_FACTOR:
-		zoom_factor+=Vector2(0.1,0.1)
-		camera.set_zoom(zoom_factor)
-	if event.is_action("scroll_up") and zoom_factor >= MIN_ZOOM_FACTOR:
-		zoom_factor-=Vector2(0.1,0.1)
-		camera.set_zoom(zoom_factor)
+
+	
 
 func _physics_process(delta):
-	
-	#WeaponController.shoot(_state)
-	
 	if relative_controls:
 		get_newtonian_input(delta)
 	else:
 		get_newtonian_input_fixed(delta)
 		
-	#fire_delay-=delta
 	global_position-=move_and_slide(velocity*speed*delta)
 
 
--- scripts/Weapons.gd
+++ scripts/Weapons.gd
@@ -10,7 +10,11 @@ const Laser =preload("res://scenes/Sustained_Gun.tscn")
 	
 var gun_left
 var gun_right
+var left_gun_false_flag = false
+var right_gun_false_flag = false
 		
+const MAX_FIRE_DELAY=0.3
+var fire_delay=0.0
 		
 func _ready() -> void:
 	gun_left = Projectile_Gun.instance()
@@ -18,61 +22,54 @@ func _ready() -> void:
 	get_tree().get_root().call_deferred("add_child", gun_right)
 	get_tree().get_root().call_deferred("add_child", gun_left)
 	
-	
-func fire_weapon(weapon):
-	if weapon=="red":
-		_projectile_left()
-	elif weapon=="blue":
-		_projectile_right()
-	elif weapon=="double":
-		_projectile_double()
+var _left_weapon=""
+var _right_weapon=""
+var _double_weapon=""
 
-"""func _ready() -> void:
-	gun_left = Projectile.instance()
-	gun_right = Laser.instance()
-	get_tree().get_root().add_child(gun_left)
-	get_tree().get_root().add_child(gun_right)
-	"""
+func _get_weapons():
+	PlayerLoadout._set_default()
+	_left_weapon=PlayerLoadout.left_weapon
+	_right_weapon=PlayerLoadout.right_weapon
+	_double_weapon=PlayerLoadout.double_weapon
+
+func shoot(state):
+	_get_weapons()
+	#print("passedState:" + state as String)
+	#print("waitingState" + _state as String)
+	match state:
+		1:
+			Weapons.fire_weapon(_left_weapon) #Calls global Weapon Script
+		2:
+			Weapons.fire_weapon(_right_weapon)
+
+		3:
+			if fire_delay<=0.0:
+				Weapons.fire_weapon(_double_weapon)
+				fire_delay=MAX_FIRE_DELAY
+		0:
+			return
 
 func _physics_process(delta: float) -> void:
-	var player = get_tree().get_root().get_node_or_null("Instance/Player_Ship")
-	
-	if player:
-#		if not player.is_connected("state_idle", self, "_on_state_idle"):
-#			player.connect("state_idle", self, "_on_state_idle")
-		if not player.is_connected("stop_fire_double", self, "_on_stop_fire_double"):
-			player.connect("stop_fire_double", self, "_on_stop_fire_double")
-		if not player.is_connected("stop_fire_left", self, "_on_stop_fire_left"):
-			player.connect("stop_fire_left", self, "_on_stop_fire_left")
-		if not player.is_connected("stop_fire_right", self, "_on_stop_fire_right"):
-			player.connect("stop_fire_right", self, "_on_stop_fire_right")
+	fire_delay-=delta
 		
-	
-
-#func _on_state_idle():
-#	if false_flag == false:
-#
-#		false_flag = true
-#		#gun_right.shoot(false)
-#		#gun_left.shoot(false)
-		
-func _on_stop_fire_double():
-		return
-	
-func _on_stop_fire_left():
+func stop_fire() -> void:
+	if not left_gun_false_flag:
+		left_gun_false_flag = true
 		gun_left.shoot(false)
-		
-func _on_stop_fire_right():
+	if not right_gun_false_flag:
+		right_gun_false_flag = true
 		gun_right.shoot(false)
 		
-#Right now harcoded to the three scenes Projectile, ProjectileRight and ProjectileDouble
-func _projectile_left():
+#Right now harcoded to the three scenes Projectile, ProjectileRight and 
+func fire_left():
+	left_gun_false_flag = false
 	gun_left.shoot(true)
 
-func _projectile_right():
+func fire_right():
+	right_gun_false_flag = false
 	gun_right.shoot(true)
 
-func _projectile_double():
+func fire_double():
 	var p = ProjectileDouble.instance()
 	get_tree().get_root().add_child(p)
 	p.transform = get_tree().get_root().get_node("Instance/Player_Ship/gun_position").global_transform
